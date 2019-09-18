847ba86ff0dc16c3ce23a0ef4b2e66a1528e9e5b
Guimaj <deco-jorge@hotmail.com>
2019-09-18 00:58:29 -0300

    código fonte

--- main.c
+++ main.c
@@ -4,59 +4,59 @@
 #include <locale.h>
 
 /*
- André Guimarães Jorge - R.A: 22118115-9
- João Victor da Silva Couto - R.A: 22118022-7
+ AndrÃ© GuimarÃ£es Jorge - R.A: 22118115-9
+ JoÃ£o Victor da Silva Couto - R.A: 22118022-7
 */
 
-int **leituraMatriz(int n){
-    int i,**m;
-    m = malloc(sizeof(int *)*n);
-    for(i=0;i<n;i++){
-        m[i] = malloc(sizeof(int)*n);
+int **leituraMatriz(int linhas){
+    int i,**matriz;
+    matriz = malloc(sizeof(int *)*linhas);
+    for(i=0;i<linhas;i++){
+        matriz[i] = malloc(sizeof(int)*linhas);
     }
-    return m;
+    return matriz;
 }
 
-void desalocaMatriz(int **m, int n){
+void desalocaMatriz(int **matriz, int linhas){
     int i;
-    for(i=0;i<n;i++){
-        free(m[i]);
+    for(i=0;i<linhas;i++){
+        free(matriz[i]);
     }
-    free(m);
+    free(matriz);
 }
 
-int** multi_matriz(int **m1,int **m2, int numv){
+int** multi_matriz(int **m1,int **m2, int numVertices){
     int i,j,k;
     int soma;
-    int **mr = leituraMatriz(numv);
-    for (i=0;i<numv;i++){
-        for(j=0;j<numv;j++){
+    int **m_resultante = leituraMatriz(numVertices);
+    for (i=0;i<numVertices;i++){
+        for(j=0;j<numVertices;j++){
             soma = 0;
-            for(k=0;k<numv;k++){
+            for(k=0;k<numVertices;k++){
                 soma += m1[i][k] * m2[k][j];
             }
-            mr[i][j] = soma;
+            m_resultante[i][j] = soma;
         }
     }
-    return mr;
+    return m_resultante;
 }
 
-void passeio(int **m_cubo, int numv){
+void passeio(int **m_cubo, int numVertices){
     int i,j;
     int maior = 0;
     
-    for(i=0;i<numv;i++){
-        for(j=i;j<numv;j++){
+    for(i=0;i<numVertices;i++){
+        for(j=i;j<numVertices;j++){
             if(m_cubo[i][j] > maior){
                 maior = m_cubo[i][j];
             }  
         }
     }
-    printf("O maior numero de passeios de tamanho 3 no grafo é %d\n",maior);
+    printf("O maior numero de passeios de tamanho 3 no grafo Ã© %d\n",maior);
     if(maior > 0){
-    	printf("Ocorrem entre os vértices:\n");
-	    for(i=0;i<numv;i++){
-	        for(j=i;j<numv;j++){
+    	printf("Ocorrem entre os vÃ©rtices:\n");
+	    for(i=0;i<numVertices;i++){
+	        for(j=i;j<numVertices;j++){
 	            if(m_cubo[i][j] == maior){
 	                printf("v%d e v%d\n",i+1,j+1);
 	            }
@@ -65,19 +65,11 @@ void passeio(int **m_cubo, int numv){
 	}
 }
 
-void graus(int **m, int numv){
-    int i,j,soma,aux;
-    int vetor_graus[numv];
-    for(i=0;i<numv;i++){
-            soma = 0;
-        for(j=0;j<numv;j++){
-            soma += m[i][j];
-        }
-        vetor_graus[i] = soma;
-    }
 
-    for(i=0;i<numv;i++){
-        for(j=0;j<numv-1;j++){
+void bubbleSort(int *vetor_graus, int numVertices){
+    int aux,i,j = 0;
+    for(i=0;i<numVertices;i++){
+        for(j=0;j<numVertices-1;j++){
             if(vetor_graus[j] < vetor_graus[j+1]){
                 aux = vetor_graus[j];
                 vetor_graus[j] = vetor_graus[j+1];
@@ -85,50 +77,67 @@ void graus(int **m, int numv){
             }
         }
     }
-    printf("Graus do grafo em ordem não crescente: ");
-    for(i=0;i<numv;i++){
+}
+
+
+
+void graus(int **matriz, int numVertices){
+    int i,j,soma;
+    int vetor_graus[numVertices];
+    for(i=0;i<numVertices;i++){
+            soma = 0;
+        for(j=0;j<numVertices;j++){
+            soma += matriz[i][j];
+        }
+        vetor_graus[i] = soma;
+    }
+
+    bubbleSort(vetor_graus, numVertices);
+
+    printf("Graus do grafo em ordem nÃ£o crescente: ");
+    for(i=0;i<numVertices;i++){
         printf("%d  ",vetor_graus[i]);
     }
     puts("");
 }
 
-int contaArestas(int **m, int numv){
+int contaArestas(int **matriz, int numVertices){
     int i,j;
-    int a = 0;
+    int arestas = 0;
 
-    for(i=0;i<numv;i++){
-       for(j=i;j<numv;j++){
-            a+=m[i][j];
+    for(i=0;i<numVertices;i++){
+       for(j=i;j<numVertices;j++){
+            arestas += matriz[i][j];
        }
     }
-    printf("Arestas do grafo: %d\n",a);
-    return a;
+    printf("Arestas do grafo: %d\n",arestas);
+    return arestas;
 }
 
-void dfs(int **adj, int numv, int ini, int *visitado, int cont){
+void dfs(int **adj, int numVertices, int ini, int *visitado, int cont){
     int i,j;
     
     visitado[ini] = cont;
 
-    for(i=0;i<numv;i++){
+    for(i=0;i<numVertices;i++){
         if(adj[ini][i] != -1){
             if(!visitado[adj[ini][i]]){
-                dfs(adj,numv,adj[ini][i],visitado,cont+1);
+                dfs(adj,numVertices,adj[ini][i],visitado,cont+1);
             }
         }
     }
 }
 
 
-int buscaGrafo(int ini, int *visitado, int numv, int **adj){
+int buscaGrafo(int ini, int *visitado, int numVertices, int **adj){
     int cont = 1;
     int i;
-    for(i=0;i<numv;i++){
+    for(i=0;i<numVertices;i++){
         visitado[i] = 0;
     }
-    dfs(adj,numv,ini,visitado,cont);
+    dfs(adj,numVertices,ini,visitado,cont);
 
-    for(i=0;i<numv;i++){
+    for(i=0;i<numVertices;i++){
         if(visitado[i] == 0){
             return 1;
         }
@@ -137,25 +146,25 @@ int buscaGrafo(int ini, int *visitado, int numv, int **adj){
 
 }
 
-void tipoGrafo(int **m, int numv, int a, int conexo){
+void tipoGrafo(int **matriz, int numVertices, int arestas, int conexo){
     int i,j,soma = 0;
     int arestamult = 0;
     int laco = 0;
 
 
-    for(i=0;i<numv-1;i++){
-        for(j=1+i;j<numv;j++){
-            if(m[i][j] > 1){
-                printf("Aresta múltipla entre os vértices v%d e v%d\n",i+1,j+1);
+    for(i=0;i<numVertices-1;i++){
+        for(j=1+i;j<numVertices;j++){
+            if(matriz[i][j] > 1){
+                printf("Aresta mÃºltipla entre os vÃ©rtices v%d e v%d\n",i+1,j+1);
                 arestamult = 1;
             }
         }
     }
     puts("");
-    for(i=0;i<numv;i++){
-        for(j=0;j<numv;j++){
-            if(m[i][i] != 0){
-                printf("Laço no vértice v%d\n",i+1);
+    for(i=0;i<numVertices;i++){
+        for(j=0;j<numVertices;j++){
+            if(matriz[i][i] != 0){
+                printf("LaÃ§o no vÃ©rtice v%d\n",i+1);
                 laco=1;
                 break;
             }
@@ -164,75 +173,78 @@ void tipoGrafo(int **m, int numv, int a, int conexo){
     puts("");
 
     if(laco == 0 && arestamult == 0){
-        if(a == numv *(numv - 1)/2){
-            printf("O grafo é completo, pois é um grafo simples onde todo vértice é adjacente a todos os outros vértices!\n");
+        if(arestas == numVertices *(numVertices - 1)/2){
+            printf("O grafo Ã© completo, pois Ã© um grafo simples onde todo vÃ©rtice Ã© adjacente a todos os outros vÃ©rtices!\n");
         }
         else{
-            printf("O grafo é simples, pois não existem arestas múltiplas ou laços!\n");
+            printf("O grafo Ã© simples, pois nÃ£o existem arestas mÃºltiplas ou laÃ§os!\n");
         }
     }
     else{
-        printf("O grafo não é simples!\n");
+        printf("O grafo nÃ£o Ã© simples!\n");
     }
 
     if(conexo == 0){
-        printf("O grafo é conexo!\n");
+        printf("O grafo Ã© conexo!\n");
         if(laco == 0 && arestamult ==0){
-        	if(a == numv - 1){
-            	printf("O grafo é uma árvore!\n");
+        	if(arestas == numVertices - 1){
+            	printf("O grafo Ã© uma Ã¡rvore!\n");
         	}	
 		}
     }
     else{
-        printf("O grafo é desconexo!\n");
+        printf("O grafo Ã© desconexo!\n");
     }
 
 }
 
+FILE* abreArquivo(){
+    FILE *arq;
+     if((arq = fopen("A.txt","r")) == NULL){
+        printf("Erro na abertura do arquivo!");
+        exit(1);
+    }
+    return arq;
+}
+
 int main(int argc, char *argv[]) {
     setlocale(LC_ALL,"Portuguese");
-    int i,j,numv,arestas,num,cont = 0;
+    int i,j,numVertices,arestas,cont,num = 0;
     int conexo;
     int **matriz;
     int **matriz_quad;
     int **matriz_cubo;
-    FILE *arq;
-
-    if((arq = fopen("A.txt","r")) == NULL){
-        printf("Erro na abertura do arquivo!");
-        exit(1);
-        system("pause");
-    }
+    FILE* arq = abreArquivo();
 
     while((fscanf(arq,"%d",&num))!= EOF){
         cont++;
     }
     rewind(arq);
-    numv = sqrt(cont);
-    matriz = leituraMatriz(numv);
-
+    numVertices = sqrt(cont); 
+    matriz = leituraMatriz(numVertices);
+    
     printf("========= Algoritmo de Grafos ============\n\n");
-    printf(" Análise do Grafo:\n\n");
+    printf(" AnÃ¡lise do Grafo:\n\n");
 
-    for(i=0;i<numv;i++){
-        for(j=0;j<numv;j++){
+    for(i=0;i<numVertices;i++){
+        for(j=0;j<numVertices;j++){
             fscanf(arq,"%d",&matriz[i][j]);
         }
     }
-    matriz_quad = multi_matriz(matriz,matriz,numv);
-    matriz_cubo = multi_matriz(matriz,matriz_quad,numv);
+    matriz_quad = multi_matriz(matriz,matriz,numVertices);
+    matriz_cubo = multi_matriz(matriz,matriz_quad,numVertices);
     
-    int visitado[numv];
-    int**adj = leituraMatriz(numv);
+    int visitado[numVertices];
+    int**adj = leituraMatriz(numVertices);
 
-    for(i=0;i<numv;i++){
-        for(j=0;j<numv;j++){
+    for(i=0;i<numVertices;i++){
+        for(j=0;j<numVertices;j++){
             adj[i][j] = -1;
         }
     }
 
-    for(i=0;i<numv;i++){
-        for(j=0;j<=numv;j++){
+    for(i=0;i<numVertices;i++){
+        for(j=0;j<=numVertices;j++){
             if(matriz[i][j] != 0){
                 adj[i][j] = j;
             }
@@ -240,22 +252,18 @@ int main(int argc, char *argv[]) {
     }
     fclose(arq);
 
-    conexo = buscaGrafo(0,visitado,numv,adj);
+    conexo = buscaGrafo(0,visitado,numVertices,adj);
     puts("");
-    graus(matriz,numv);
+    graus(matriz,numVertices);
     puts("");
-    arestas = contaArestas(matriz,numv);
+    arestas = contaArestas(matriz,numVertices);
     puts("");
-    tipoGrafo(matriz,numv,arestas,conexo);
+    tipoGrafo(matriz,numVertices,arestas,conexo);
     puts("");
-    passeio(matriz_cubo,numv);
-    desalocaMatriz(matriz_quad,numv);
-    desalocaMatriz(matriz_cubo,numv);
-    desalocaMatriz(matriz,numv);
+    passeio(matriz_cubo,numVertices);
+    desalocaMatriz(matriz_quad,numVertices);
+    desalocaMatriz(matriz_cubo,numVertices);
+    desalocaMatriz(matriz,numVertices);
     system("pause");
     return 0;
 }
-
-
-
-
